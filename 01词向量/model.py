715cb6fbfa036dff75a8833ab9697f948939ad8b
qqh0618 <1365648293@qq.com>
2024-08-10 15:18:23 +0800

    词向量初始化

---
+++ 01词向量/model.py
@@ -0,0 +1,16 @@
+"""
+2024/8/9 16:21
+本文件由my_ywj首次创建编写
+"""
+import numpy
+import torch
+import torch.nn as nn
+
+
+class WordVec(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+
+    def forward(self, x):
+        pass
